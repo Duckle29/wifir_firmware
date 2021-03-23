b3e60f67bfc8e26736d32bbcffca3578d12db803
Mikkel Jeppesen <2756925+Duckle29@users.noreply.github.com>
2021-03-23 18:44:42 +0100

    Don't exit the entire build process if the certs are fresh ;)

--- fetch_certs_and_upload.py
+++ fetch_certs_and_upload.py
@@ -1,20 +1,17 @@
 #!/usr/bin/env python3
 from pathlib import Path
 from time import time
-from sys import exit
 
 Import("env")
 
 certbundle = Path(env["PROJECT_DIR"]) / "data" / "certs.ar"
 
-if certbundle.is_file():
+try:
     # If certificate bundle is more than 30 days, fetch and upload a new image
     if time() - certbundle.stat().st_mtime < 30 * 24 * 60 * 60:
         print("Certificate bundle is still recent. No change")
-        exit(0)
-else:
-    if not certbundle.parent.is_dir():
-        certbundle.parent.mkdir()
-
-env.Execute("$PYTHONEXE get_moz_certs.py")
-env.Execute("$PYTHONEXE -m platformio run --target uploadfs")
+    else:
+        raise FileNotFoundError("A wee hack")
+except FileNotFoundError:
+    env.Execute("$PYTHONEXE get_moz_certs.py")
+    env.Execute("$PYTHONEXE -m platformio run --target uploadfs")
