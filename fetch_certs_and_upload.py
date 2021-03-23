99de3a76659844c3fef3486906f408d5eeb45cfb
Mikkel Jeppesen <2756925+Duckle29@users.noreply.github.com>
2021-03-23 17:08:41 +0100

    Remember -m

--- fetch_certs_and_upload.py
+++ fetch_certs_and_upload.py
@@ -1,20 +1,20 @@
 #!/usr/bin/env python3
-Import("env")
-
 from pathlib import Path
 from time import time
 from sys import exit
+
+Import("env")
 
 certbundle = Path(env["PROJECT_DIR"]) / "data" / "certs.ar"
 
 if certbundle.is_file():
     # If certificate bundle is more than 30 days, fetch and upload a new image
-    if time() - certbundle.stat().st_mtime > 30 * 24 * 60 * 60:
+    if time() - certbundle.stat().st_mtime < 30 * 24 * 60 * 60:
         print("Certificate bundle is still recent. No change")
         exit(0)
 else:
     if not certbundle.parent.is_dir():
         certbundle.parent.mkdir()
 
-env.Execute("$PYTHONEXE gen_moz_certs.py")
-env.Execute("$PYTHONEXE pio run --target uploadfs")
+env.Execute("$PYTHONEXE get_moz_certs.py")
+env.Execute("$PYTHONEXE -m platformio run --target uploadfs")
